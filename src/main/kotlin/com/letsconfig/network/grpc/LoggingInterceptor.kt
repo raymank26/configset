451a21c51ba66a452284a221d86d88bd16fdda84
Anton Ermak <rayman.k26@gmail.com>
2020-04-11 23:59:58 +0300

    Logging added.

--- src/main/kotlin/com/letsconfig/network/grpc/LoggingInterceptor.kt
+++ src/main/kotlin/com/letsconfig/network/grpc/LoggingInterceptor.kt
@@ -1,42 +1,30 @@
 package com.letsconfig.network.grpc
 
 import com.letsconfig.extension.createLogger
-import io.grpc.ForwardingServerCall.SimpleForwardingServerCall
+import io.grpc.ForwardingServerCallListener
 import io.grpc.Metadata
 import io.grpc.ServerCall
 import io.grpc.ServerCallHandler
 import io.grpc.ServerInterceptor
 import io.grpc.Status
-import java.io.PrintWriter
-
-import java.io.StringWriter
 
 class LoggingInterceptor : ServerInterceptor {
     private val log = createLogger()
 
     override fun <ReqT, RespT> interceptCall(call: ServerCall<ReqT, RespT>, headers: Metadata, next: ServerCallHandler<ReqT, RespT>): ServerCall.Listener<ReqT> {
-        val wrapper = object : SimpleForwardingServerCall<ReqT, RespT>(call) {
+        val listener: ServerCall.Listener<ReqT> = next.startCall(call, headers)
 
-            override fun close(status: Status, trailers: Metadata) {
-                val newStatus = if (status.code === Status.Code.UNKNOWN && status.description == null && status.cause != null) {
-                    val e = status.cause
-                    log.info("Exception during request processing", e);
-                    Status.INTERNAL
-                            .withDescription(e!!.message)
-                            .augmentDescription(stacktraceToString(e))
-                } else {
-                    status
+        return object : ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(listener) {
+            override fun onHalfClose() {
+                try {
+                    super.onHalfClose()
+                } catch (e: Exception) {
+                    log.warn("Exception occurred for method = ${call.methodDescriptor.fullMethodName}", e)
+                    call.close(Status.INTERNAL
+                            .withCause(e)
+                            .withDescription("error message"), Metadata())
                 }
-                super.close(newStatus, trailers)
             }
         }
-        return next.startCall(wrapper, headers);
-    }
-
-    private fun stacktraceToString(e: Throwable): String? {
-        val stringWriter = StringWriter()
-        val printWriter = PrintWriter(stringWriter)
-        e.printStackTrace(printWriter)
-        return stringWriter.toString()
     }
 }
