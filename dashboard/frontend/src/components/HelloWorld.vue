4198c8f64921a95ef9fe43f3b18a0175fcfa260d
Anton Ermak <rayman.k26@gmail.com>
2020-04-26 13:42:20 +0300

    Hello world dashboard.

--- dashboard/frontend/src/components/HelloWorld.vue
+++ dashboard/frontend/src/components/HelloWorld.vue
@@ -34,12 +34,19 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+  import {Component, Prop, Vue} from 'vue-property-decorator'
+  import Axios from 'axios'
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+  @Component
+  export default class HelloWorld extends Vue {
+    @Prop() private msg!: string;
+
+    created() {
+      Axios.get('/api/hello').then(response => {
+        alert(response)
+      })
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- dashboard/src/main/kotlin/com/letsconfig/dashboard/Main.kt
+++ dashboard/src/main/kotlin/com/letsconfig/dashboard/Main.kt
@@ -1,59 +1,13 @@
 package com.letsconfig.dashboard
 
-import com.googlecode.lanterna.TerminalSize
-import com.googlecode.lanterna.TextColor
-import com.googlecode.lanterna.gui2.BasicWindow
-import com.googlecode.lanterna.gui2.Button
-import com.googlecode.lanterna.gui2.DefaultWindowManager
-import com.googlecode.lanterna.gui2.EmptySpace
-import com.googlecode.lanterna.gui2.GridLayout
-import com.googlecode.lanterna.gui2.Label
-import com.googlecode.lanterna.gui2.MultiWindowTextGUI
-import com.googlecode.lanterna.gui2.Panel
-import com.googlecode.lanterna.gui2.TextBox
-import com.googlecode.lanterna.screen.Screen
-import com.googlecode.lanterna.screen.TerminalScreen
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory
-import com.googlecode.lanterna.terminal.Terminal
+import io.javalin.Javalin
 
 
 object Main {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        // Setup terminal and screen layers
-
-        // Setup terminal and screen layers
-        val terminal: Terminal = DefaultTerminalFactory().setForceTextTerminal(true).createTerminal()
-        val screen: Screen = TerminalScreen(terminal)
-        screen.startScreen()
-
-        // Create panel to hold components
-
-        // Create panel to hold components
-        val panel = Panel()
-        panel.setLayoutManager(GridLayout(2))
-
-        panel.addComponent(Label("Forename"))
-        panel.addComponent(TextBox())
-
-        panel.addComponent(Label("Surname"))
-        panel.addComponent(TextBox())
-
-        panel.addComponent(EmptySpace(TerminalSize(0, 0))) // Empty space underneath labels
-
-        panel.addComponent(Button("Submit"))
-
-        // Create window to hold the panel
-
-        // Create window to hold the panel
-        val window = BasicWindow()
-        window.component = panel
-
-        // Create gui and start gui
-
-        // Create gui and start gui
-        val gui = MultiWindowTextGUI(screen, DefaultWindowManager(), EmptySpace(TextColor.ANSI.BLUE))
-        gui.addWindowAndWait(window)
+        val app = Javalin.create().start(8188)
+        app.get("/api/hello") { ctx -> ctx.result("Hello World") }
     }
 }
