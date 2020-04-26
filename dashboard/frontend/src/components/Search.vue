4e5444bcfff7269513b76e2cb6894baea5318729
Anton Ermak <rayman.k26@gmail.com>
2020-04-26 15:51:31 +0300

    Select applications.

--- dashboard/frontend/src/components/Search.vue
+++ dashboard/frontend/src/components/Search.vue
@@ -5,11 +5,11 @@
         <div class="form-row">
           <div class="col">
             <label class="mr-sm-2 sr-only" for="appSelect">Application</label>
-            <select class="custom-select" id="appSelect">
-              <option selected>Select application</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+            <select class="custom-select" id="appSelect" v-model="searchApplicationName">
+              <option selected value="">Select application</option>
+              <option>One</option>
+              <option>Two</option>
+              <option>Three</option>
             </select>
           </div>
           <div class="col">
