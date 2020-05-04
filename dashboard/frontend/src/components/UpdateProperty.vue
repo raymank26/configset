ae36f27535e4e8d57475c25f2fe70dd6617ac53c
Anton Ermak <rayman.k26@gmail.com>
2020-05-05 01:14:47 +0300

    Error handling.

--- dashboard/frontend/src/components/UpdateProperty.vue
+++ dashboard/frontend/src/components/UpdateProperty.vue
@@ -1,3 +1,4 @@
+import {UpdateResult} from "@/service/PropertyService";
 <template>
   <div>
     <div class="row">
@@ -5,45 +6,62 @@
         <h4>Create/Update property</h4>
       </div>
     </div>
-    <div class="row" v-if="!loading">
-      <div class="col-4">
-        <form @submit="submitApp" novalidate ref="appForm">
-          <div class="form-group">
-            <label for="appName">Application name</label>
-            <select class="custom-select" id="appName" required v-model="appName">
-              <option value="">Select application</option>
-              <option v-for="app in applications">{{ app }}</option>
-            </select>
-            <div class="invalid-feedback">
-              Application is not selected
+    <div v-if="!loading">
+      <div class="row">
+        <div class="col-4">
+          <form @submit="submitApp" novalidate ref="appForm">
+            <div class="form-group">
+              <label for="appName">Application name</label>
+              <select class="custom-select" id="appName" required v-model="appName">
+                <option value="">Select application</option>
+                <option v-for="app in applications">{{ app }}</option>
+              </select>
+              <div class="invalid-feedback">
+                Application is not selected
+              </div>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="hostName">Host name</label>
-            <input class="form-control" id="hostName" pattern="[A-Za-z-0-9.]+" required type="text" v-model="hostName">
-            <div class="invalid-feedback">
-              Invalid application name, pattern = [A-Za-z-]+
+            <div class="form-group">
+              <label for="hostName">Host name</label>
+              <input class="form-control" id="hostName" pattern="[A-Za-z-0-9.]+" required type="text"
+                     v-model="hostName">
+              <div class="invalid-feedback">
+                Invalid application name, pattern = [A-Za-z-]+
+              </div>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="propertyName">Property name</label>
-            <input class="form-control" id="propertyName" pattern="[A-Za-z-0-9.]+" required type="text"
-                   v-model="propertyName">
-            <div class="invalid-feedback">
-              Invalid application name, pattern = [A-Za-z-]+
+            <div class="form-group">
+              <label for="propertyName">Property name</label>
+              <input class="form-control" id="propertyName" pattern="[A-Za-z-0-9.]+" required type="text"
+                     v-model="propertyName">
+              <div class="invalid-feedback">
+                Invalid application name, pattern = [A-Za-z-]+
+              </div>
             </div>
-          </div>
-          <div class="form-group">
-            <label for="propertyValue">Property value</label>
-            <textarea class="form-control" id="propertyValue" type="text" v-model="propertyValue"/>
-            <div class="invalid-feedback">
-              Invalid application name, pattern = [A-Za-z-]+
+            <div class="form-group">
+              <label for="propertyValue">Property value</label>
+              <textarea class="form-control" id="propertyValue" type="text" v-model="propertyValue"/>
+              <div class="invalid-feedback">
+                Invalid application name, pattern = [A-Za-z-]+
+              </div>
             </div>
+            <div class="form-group">
+              <input class="btn btn-primary" type="submit" value="Update"/>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <div class="alert alert-danger" v-if="hasConflict">
+            The property value has conflict with already saved one. Please update the page to see the update.
           </div>
-          <div class="form-group">
-            <input class="btn btn-primary" type="submit" value="Update"/>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-6">
+          <div class="alert alert-danger" v-if="appNotFound">
+            The selected application is not found.
           </div>
-        </form>
+        </div>
       </div>
     </div>
     <div v-else>
@@ -66,7 +84,10 @@
     hostName: string = "";
     propertyValue: string = "";
     _version: number | null = null;
+
     loading: boolean = true;
+    hasConflict: boolean = false;
+    appNotFound: boolean = false;
 
     created() {
       if (this.$router.currentRoute.query) {
@@ -90,23 +111,23 @@
     }
 
     submitApp(e: any) {
-      let form = this.$refs['appForm'] as HTMLFormElement;
-      let isValid = form.checkValidity();
-      form.classList.add('was-validated');
-
-      if (isValid) {
-        propertyService.updateProperty(this.appName, this.hostName, this.propertyName, this.propertyValue, this._version)
-          .then(updateResult => {
-            switch (updateResult) {
-              case UpdateResult.OK:
-                this.$router.push("/search");
-                break;
-              case UpdateResult.CONFLICT:
-                alert("conflict");
-                break;
-            }
-          });
+      if (this.hasConflict) {
+        return;
       }
+      propertyService.updateProperty(this.appName, this.hostName, this.propertyName, this.propertyValue, this._version)
+        .then(updateResult => {
+          switch (updateResult) {
+            case UpdateResult.OK:
+              this.$router.push("/search");
+              break;
+            case UpdateResult.CONFLICT:
+              this.hasConflict = true;
+              break;
+            case UpdateResult.APPLICATION_NOT_FOUND:
+              this.appNotFound = true;
+              break;
+          }
+        });
     }
   }
 </script>
