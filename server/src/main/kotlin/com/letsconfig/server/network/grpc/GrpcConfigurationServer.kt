d727ad25f5791b8a3f0dee0b1121c6958587a6cb
Anton Ermak <rayman.k26@gmail.com>
2020-08-05 04:49:31 +0300

    Keepalive fix.

--- server/src/main/kotlin/com/letsconfig/server/network/grpc/GrpcConfigurationServer.kt
+++ server/src/main/kotlin/com/letsconfig/server/network/grpc/GrpcConfigurationServer.kt
@@ -1,12 +1,16 @@
 package com.letsconfig.server.network.grpc
 
 import io.grpc.Server
-import io.grpc.ServerBuilder
 import io.grpc.ServerInterceptors
+import io.grpc.netty.NettyServerBuilder
+import java.util.concurrent.TimeUnit
 
 class GrpcConfigurationServer(grpcConfigurationService: GrpcConfigurationService, port: Int = 8080) {
 
-    private val server: Server = ServerBuilder.forPort(port)
+    // For keepalive see https://stackoverflow.com/questions/57606303/grpc-arg-keepalive-permit-without-calls-for-java-servers
+    private val server: Server = NettyServerBuilder.forPort(port)
+            .permitKeepAliveWithoutCalls(true)
+            .permitKeepAliveTime(4, TimeUnit.SECONDS)
             .addService(ServerInterceptors.intercept(grpcConfigurationService, LoggingInterceptor()))
             .build()
 
