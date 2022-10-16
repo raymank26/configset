2b10175bac521d522d0ca192bdea3c3fdccc44a1
Anton Ermak <rayman.k26@gmail.com>
2022-10-16 11:50:45 +0700

    Renaming of user.

--- dashboard/src/functionalTest/kotlin/com/configset/dashboard/FunctionalTest.kt
+++ dashboard/src/functionalTest/kotlin/com/configset/dashboard/FunctionalTest.kt
@@ -76,7 +76,7 @@ abstract class FunctionalTest {
                 override fun authenticationProvider(objectMapper: ObjectMapper): AuthenticationProvider {
                     return stubAuthenticationProvider {
                         addUser(EMPTY_ROLES_ACCESS_TOKEN, "test.user", setOf())
-                        addUser(FULL_ROLES_ACCESS_TOKEN, "test.user", setOf("applicationCreator"))
+                        addUser(FULL_ROLES_ACCESS_TOKEN, "full.test.user", setOf("applicationCreator"))
                     }
                 }
             })
