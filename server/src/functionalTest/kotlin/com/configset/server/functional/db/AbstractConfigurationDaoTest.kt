0f597d29bdc3633a28c714148e49bbb17fb61a74
Anton Ermak <rayman.k26@gmail.com>
2022-10-16 23:32:59 +0700

    Refactoring.

--- server/src/functionalTest/kotlin/com/configset/server/functional/db/AbstractConfigurationDaoTest.kt
+++ server/src/functionalTest/kotlin/com/configset/server/functional/db/AbstractConfigurationDaoTest.kt
@@ -7,7 +7,6 @@ import com.configset.server.SearchPropertyRequest
 import com.configset.server.db.ConfigurationDao
 import com.configset.server.db.DbHandleFactory
 import com.configset.server.db.PropertyItemED
-import com.configset.server.db.common.DbHandle
 import com.configset.server.fixtures.TEST_APP_NAME
 import com.configset.server.fixtures.TEST_HOST
 import org.amshove.kluent.shouldBe
@@ -32,11 +31,12 @@ abstract class AbstractConfigurationDaoTest {
     }
 
     @Test
-    fun testCreateApplication() {
+    fun `should create application`() {
         test {
-            atomic {
-                createApp()
-            }
+            // when
+            createApp()
+
+            // then
             appNames() shouldBeEqualTo listOf(TEST_APP_NAME)
         }
     }
@@ -44,9 +44,9 @@ abstract class AbstractConfigurationDaoTest {
     @Test
     fun testDeleteApplication() {
         test {
-            // given
-            atomic { createApp("test1") }
-            atomic { deleteApp("test1") }
+            // when
+            createApp("test1")
+            deleteApp("test1")
 
             // then
             appNames() shouldBeEqualTo listOf()
@@ -57,17 +57,15 @@ abstract class AbstractConfigurationDaoTest {
     fun `should not return properties on deleted applications`() {
         test {
             // given
-            atomic {
-                createApp()
-                createHost()
-                updateProperty(name = "foo", value = "bar")
-            }
-
-            // then
-            val properties = properties()
-            atomic { deleteApp(TEST_APP_NAME) }
+            createApp()
+            createHost()
+            updateProperty(name = "foo", value = "bar")
 
             // when
+            val properties = properties()
+            deleteApp(TEST_APP_NAME)
+
+            // then
             val propertiesAfterDeletion = properties()
             properties.size shouldBeEqualTo 1
             propertiesAfterDeletion.shouldBeEmpty()
@@ -78,9 +76,7 @@ abstract class AbstractConfigurationDaoTest {
     fun `should create host`() {
         test {
             // given
-            atomic {
-                createHost()
-            }
+            createHost()
 
             // when
             appHosts() shouldBeEqualTo listOf(TEST_HOST)
@@ -88,16 +84,17 @@ abstract class AbstractConfigurationDaoTest {
     }
 
     @Test
-    fun `should create property`() {
+    fun `should update app version`() {
         test {
-            atomic {
-                createApp()
-                createHost()
-                updateProperty("name", "value")
-            }
-            atomic {
-                updateProperty("name", "valueNext", 1)
-            }
+            // given
+            createApp()
+            createHost()
+            updateProperty("name", "value")
+
+            // when
+            updateProperty("name", "valueNext", 1)
+
+            // then
             apps().first().lastVersion shouldBeEqualTo 2
         }
     }
@@ -105,185 +102,201 @@ abstract class AbstractConfigurationDaoTest {
     @Test
     fun `property update should change previously created property`() {
         test {
-            atomic {
-                createApp()
-                createHost()
-                updateProperty("name", "value")
-                updateProperty("foobar", "value2")
-            }
-            atomic {
-                readProperty("name")
-                updateProperty("name", "value1")
-            }
+            // given
+            createApp()
+            createHost()
+
+            // when
+            updateProperty("name", "value")
+            updateProperty("name", "value1")
+
+            // then
+            val searchRes: List<PropertyItemED> = dao.searchProperties(
+                SearchPropertyRequest(
+                    TEST_APP_NAME,
+                    null, null, null
+                )
+            )
+            searchRes.find { it.name == "name" }!!.value shouldBeEqualTo "value1"
         }
-        val searchRes: List<PropertyItemED> =
-            dao.searchProperties(SearchPropertyRequest(TEST_APP_NAME, null, null, null))
-        searchRes.find { it.name == "name" }!!.value shouldBeEqualTo "value1"
-        searchRes.find { it.name == "foobar" }!!.value shouldBeEqualTo "value2"
     }
 
     @Test
     fun `property should be deleted`() {
         test {
-            atomic {
-                createApp()
-                createHost()
-                updateProperty("name", "value")
-                deleteProperty("name") shouldBeEqualTo DeletePropertyResult.OK
-            }
-        }
-        dao.listProperties(TEST_APP_NAME) shouldBeEqualTo listOf()
+            // given
+            createApp()
+            createHost()
+            updateProperty("name", "value")
 
-        val snapshot = dao.getConfigurationSnapshotList()
-        snapshot.size shouldBeEqualTo 1
+            // when
+            deleteProperty("name") shouldBeEqualTo DeletePropertyResult.OK
 
-        val first = snapshot.first()
-        first.deleted shouldBe true
-        first.version shouldBeEqualTo 2
-    }
+            // then
+            dao.listProperties(TEST_APP_NAME) shouldBeEqualTo listOf()
 
-    @Test
-    fun testDeleteConflict() {
-        test {
-            atomic {
-                createApp()
-                createHost()
-            }
-            atomic {
-                updateProperty("name", "value")
-                deleteProperty("name", version = 123) shouldBeEqualTo DeletePropertyResult.DeleteConflict
-            }
-        }
-        dao.listProperties(TEST_APP_NAME).size shouldBeEqualTo 1
-    }
+            val snapshot = dao.getConfigurationSnapshotList()
+            snapshot.size shouldBeEqualTo 1
 
-    @Test
-    fun testUpdateAfterDelete() {
-        test {
-            atomic {
-                createApp()
-                createHost()
-            }
-            atomic {
-                updateProperty("name", "value")
-                deleteProperty("name")
-                updateProperty("name", "value") // TODO: think about null version!
-            }
-        }
-        dao.searchProperties(SearchPropertyRequest(TEST_APP_NAME, "name", null, null)).first().version shouldBeEqualTo 3
-    }
+            val first = snapshot.first()
+            first.deleted shouldBe true
+            first.version shouldBeEqualTo 2
 
-    @Test
-    fun listProperties() {
-        test {
-            atomic {
-                createApp()
-                createApp("test-app2")
-                createHost()
-                createHost("srvd2")
-                updateProperty("name", "value")
-                updateProperty("name", "value", host = "srvd2")
-                updateProperty("name", "value", appName = "test-app2", host = "srvd2")
-            }
-        }
-
-        dao.listProperties(TEST_APP_NAME) shouldBeEqualTo listOf("name")
-    }
-
-    @Test
-    fun searchProperties() {
-        test {
-            atomic {
-                createApp()
-                createApp("test-app2")
-                createHost()
-                createHost("srvd2")
-                updateProperty("name", "value")
-                updateProperty("name2", "value2")
-                updateProperty("name1", "value", host = "srvd2")
-            }
-        }
-
-        dao.searchProperties(SearchPropertyRequest(null, "Nam", "Val", "SRVD1"))
-            .map { it.name } shouldBeEqualTo listOf("name", "name2")
-        dao.searchProperties(SearchPropertyRequest(null, "nam", "val", "srvd"))
-            .map { it.name } shouldBeEqualTo listOf("name", "name2", "name1")
-    }
-
-    @Test
-    fun readProperty() {
-        test {
-            atomic {
-                createApp()
-                createHost()
-                createHost("srvd2")
-                createHost("srvd3")
-                updateProperty("name", "value")
-                updateProperty("name", "value2", host = "srvd2")
-                updateProperty("name", "value3", host = "srvd3")
-            }
-        }
-
-        dao.readProperty(TEST_APP_NAME, TEST_HOST, "name")!!.apply {
-            hostName shouldBeEqualTo "srvd1"
-            applicationName shouldBeEqualTo TEST_APP_NAME
-            name shouldBeEqualTo "name"
-            value shouldBeEqualTo "value"
-            version shouldBeEqualTo 1
-        }
-
-        dao.readProperty(TEST_APP_NAME, "srvd2", "name")!!.apply {
-            hostName shouldBeEqualTo "srvd2"
-            applicationName shouldBeEqualTo TEST_APP_NAME
-            name shouldBeEqualTo "name"
-            value shouldBeEqualTo "value2"
-            version shouldBeEqualTo 2
-        }
-
-        dao.readProperty(TEST_APP_NAME, "srvd3", "name")!!.apply {
-            hostName shouldBeEqualTo "srvd3"
-            applicationName shouldBeEqualTo TEST_APP_NAME
-            name shouldBeEqualTo "name"
-            value shouldBeEqualTo "value3"
-            version shouldBeEqualTo 3
         }
     }
 
     @Test
-    fun testSnapshot() {
+    fun `should return conflict error on property deletion`() {
         test {
-            atomic {
-                createApp()
-                createHost()
-                createHost("srvd2")
-                updateProperty("name", "value")
-                updateProperty("name2", "value2")
-                updateProperty("name", "value2", host = "srvd2")
-            }
+            // given
+            createApp()
+            createHost()
+
+            // when
+            updateProperty("name", "value")
+            deleteProperty("name", version = 123) shouldBeEqualTo DeletePropertyResult.DeleteConflict
+
+            // then
+            dao.listProperties(TEST_APP_NAME).size shouldBeEqualTo 1
+        }
+    }
+
+    @Test
+    fun `should update deleted property`() {
+        test {
+            // given
+            createApp()
+            createHost()
+            updateProperty("name", "value")
+            deleteProperty("name")
+
+            // when
+            updateProperty("name", "value") // TODO: think about null version!
+
+            // then
+            val property = dao.searchProperties(SearchPropertyRequest(TEST_APP_NAME, "name", null, null)).first()
+            property.version shouldBeEqualTo 3
         }
 
-        dao.getConfigurationSnapshotList()
-            .shouldMatchAtLeastOneOf {
-                it.applicationName == TEST_APP_NAME
-                        && it.name == "name"
-                        && it.hostName == TEST_HOST
-                        && it.version == 1L
-                        && it.value == "value"
+    }
+
+    @Test
+    fun `should list properties`() {
+        test {
+            // given
+            createApp()
+            createApp("test-app2")
+            createHost()
+            createHost("srvd2")
+
+            // when
+            updateProperty("name", "value")
+            updateProperty("name", "value", host = "srvd2")
+            updateProperty("name", "value", appName = "test-app2", host = "srvd2")
+
+            // then
+            dao.listProperties(TEST_APP_NAME) shouldBeEqualTo listOf("name")
+        }
+    }
+
+    @Test
+    fun `should search properties`() {
+        test {
+            // given
+            createApp()
+            createApp("test-app2")
+            createHost()
+            createHost("srvd2")
+
+            // when
+            updateProperty("name", "value")
+            updateProperty("name2", "value2")
+            updateProperty("name1", "value", host = "srvd2")
+
+            // then
+            dao.searchProperties(SearchPropertyRequest(null, "Nam", "Val", "SRVD1"))
+                .map { it.name } shouldBeEqualTo listOf("name", "name2")
+            dao.searchProperties(SearchPropertyRequest(null, "nam", "val", "srvd"))
+                .map { it.name } shouldBeEqualTo listOf("name", "name2", "name1")
+        }
+    }
+
+    @Test
+    fun `should update property versions`() {
+        test {
+            // given
+            createApp()
+            createHost()
+            createHost("srvd2")
+            createHost("srvd3")
+
+            // when
+            updateProperty("name", "value")
+            updateProperty("name", "value2", host = "srvd2")
+            updateProperty("name", "value3", host = "srvd3")
+
+            // then
+            dao.readProperty(TEST_APP_NAME, TEST_HOST, "name")!!.apply {
+                hostName shouldBeEqualTo "srvd1"
+                applicationName shouldBeEqualTo TEST_APP_NAME
+                name shouldBeEqualTo "name"
+                value shouldBeEqualTo "value"
+                version shouldBeEqualTo 1
             }
-            .shouldMatchAtLeastOneOf {
-                it.applicationName == TEST_APP_NAME
-                        && it.name == "name2"
-                        && it.hostName == TEST_HOST
-                        && it.version == 2L
-                        && it.value == "value2"
+
+            dao.readProperty(TEST_APP_NAME, "srvd2", "name")!!.apply {
+                hostName shouldBeEqualTo "srvd2"
+                applicationName shouldBeEqualTo TEST_APP_NAME
+                name shouldBeEqualTo "name"
+                value shouldBeEqualTo "value2"
+                version shouldBeEqualTo 2
             }
-            .shouldMatchAtLeastOneOf {
-                it.applicationName == TEST_APP_NAME
-                        && it.name == "name"
-                        && it.hostName == "srvd2"
-                        && it.version == 3L
-                        && it.value == "value2"
+
+            dao.readProperty(TEST_APP_NAME, "srvd3", "name")!!.apply {
+                hostName shouldBeEqualTo "srvd3"
+                applicationName shouldBeEqualTo TEST_APP_NAME
+                name shouldBeEqualTo "name"
+                value shouldBeEqualTo "value3"
+                version shouldBeEqualTo 3
             }
+        }
+    }
+
+    @Test
+    fun `should collect snapshot`() {
+        test {
+            // given
+            createApp()
+            createHost()
+            createHost("srvd2")
+            updateProperty("name", "value")
+            updateProperty("name2", "value2")
+            updateProperty("name", "value2", host = "srvd2")
+
+            // then
+            dao.getConfigurationSnapshotList()
+                .shouldMatchAtLeastOneOf {
+                    it.applicationName == TEST_APP_NAME
+                            && it.name == "name"
+                            && it.hostName == TEST_HOST
+                            && it.version == 1L
+                            && it.value == "value"
+                }
+                .shouldMatchAtLeastOneOf {
+                    it.applicationName == TEST_APP_NAME
+                            && it.name == "name2"
+                            && it.hostName == TEST_HOST
+                            && it.version == 2L
+                            && it.value == "value2"
+                }
+                .shouldMatchAtLeastOneOf {
+                    it.applicationName == TEST_APP_NAME
+                            && it.name == "name"
+                            && it.hostName == "srvd2"
+                            && it.version == 3L
+                            && it.value == "value2"
+                }
+        }
     }
 
     private fun test(f: TestDsl.() -> Unit) {
@@ -297,46 +310,46 @@ class TestDsl(
     private val dao: ConfigurationDao,
 ) {
 
-    @Deprecated("Incorrect context", level = DeprecationLevel.ERROR)
-    fun DbHandle.atomic(handle: DbHandle.() -> Unit) {
-    }
-
-    fun atomic(handle: DbHandle.() -> Unit) {
+    fun createApp(name: String? = null) {
         dbHandleFactory.withHandle {
-            handle.invoke(it)
+            dao.createApplication(it, name ?: TEST_APP_NAME)
         }
     }
 
-    fun DbHandle.createApp(name: String? = null) {
-        dao.createApplication(this, name ?: TEST_APP_NAME)
+    fun deleteApp(name: String) {
+        dbHandleFactory.withHandle {
+            dao.deleteApplication(it, name)
+        }
     }
 
-    fun DbHandle.deleteApp(name: String) {
-        dao.deleteApplication(this, name)
+    fun createHost(name: String? = null) {
+        dbHandleFactory.withHandle {
+            dao.createHost(it, name ?: TEST_HOST) shouldBeEqualTo HostCreateResult.OK
+        }
     }
 
-    fun DbHandle.createHost(name: String? = null) {
-        dao.createHost(this, name ?: TEST_HOST) shouldBeEqualTo HostCreateResult.OK
-    }
-
-    fun DbHandle.updateProperty(
+    fun updateProperty(
         name: String,
         value: String,
         version: Long? = null,
         appName: String = TEST_APP_NAME,
         host: String = TEST_HOST,
     ) {
-        val v = version ?: readProperty(name, app = appName, host = host)?.version
-        dao.updateProperty(this, appName, name, value, v, host)
+        dbHandleFactory.withHandle {
+            val v = version ?: readProperty(name, app = appName, host = host)?.version
+            dao.updateProperty(it, appName, name, value, v, host)
+        }
     }
 
     fun readProperty(name: String, app: String = TEST_APP_NAME, host: String = TEST_HOST): PropertyItemED? {
         return dao.readProperty(app, host, name)
     }
 
-    fun DbHandle.deleteProperty(name: String, version: Long? = null): DeletePropertyResult {
+    fun deleteProperty(name: String, version: Long? = null): DeletePropertyResult {
         val v = version ?: readProperty(name)!!.version
-        return dao.deleteProperty(this, TEST_APP_NAME, TEST_HOST, name, v)
+        return dbHandleFactory.withHandle {
+            dao.deleteProperty(it, TEST_APP_NAME, TEST_HOST, name, v)
+        }
     }
 
     fun apps(): List<ApplicationED> {
