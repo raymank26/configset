4b21ab959cd5d8d40a1000e3a1dcd4a91dfc91ec
Anton Ermak <rayman.k26@gmail.com>
2022-10-25 17:02:33 +0700

    Module system.

---
+++ dashboard/src/main/resources/js/widgets.js
@@ -0,0 +1,60 @@
+export function initWidgets() {
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('autocompleteForm', () => {
+            return {
+                'value': '',
+                'autocompleteItems': [],
+                'selectedSuggestionIndex': 0,
+                init() {
+                    this.autocompleteContainerId = this.$id('autocomplete');
+
+                    let that = this;
+                    this.$el.addEventListener('autocompleteItemsUpdated', () => {
+                        let autocompleteContainer = document.getElementById(that.autocompleteContainerId);
+
+                        that.selectedSuggestionIndex = -1;
+                        that.autocompleteItems = autocompleteContainer.getElementsByTagName('div');
+                    })
+                    this.$el.addEventListener('focusout', () => {
+                        that.autocompleteItems = [];
+                        that.selectedSuggestionIndex = -1;
+                    })
+                    this.$refs.autocompleteInput.addEventListener('keydown', (e) => {
+                        // console.log(that.$el);
+                        if (e.keyCode === 9) { // tab
+                            return;
+                        }
+                        if (e.keyCode === 13) { // enter
+                            that.value = that.autocompleteItems[that.selectedSuggestionIndex].innerHTML;
+                            that.autocompleteItems = []
+                            that.selectedSuggestionIndex = -1;
+                            e.preventDefault();
+                            return;
+                        } else if (e.keyCode === 38) { // up
+                            that.selectedSuggestionIndex = Math.max(0, that.selectedSuggestionIndex - 1);
+                        } else if (e.keyCode === 40 && that.autocompleteItems.length > 0) { // down
+                            that.selectedSuggestionIndex = Math.min(that.autocompleteItems.length - 1,
+                                that.selectedSuggestionIndex + 1);
+                        } else {
+                            that.$nextTick(() => {
+                                e.target.dispatchEvent(new Event('autocompleteAjaxTrigger'));
+                            })
+                            return;
+                        }
+                        for (let item of that.autocompleteItems) {
+                            item.classList.remove('autocomplete-active');
+                        }
+                        if (that.selectedSuggestionIndex >= 0) {
+                            that.autocompleteItems[that.selectedSuggestionIndex].classList.add('autocomplete-active');
+                        }
+                    });
+                    this.$refs.autocompleteResult.addEventListener('mousedown', (e) => {
+                        that.value = e.target.innerHTML;
+                        that.autocompleteItems = [];
+                        that.selectedSuggestionIndex = -1;
+                    })
+                }
+            }
+        })
+    });
+}
