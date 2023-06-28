1aee47af03eb77f2dfa6be802e33fe0fb5199fd3
Anton Ermak <rayman.k26@gmail.com>
2023-06-28 17:35:08 +0700

    Refactoring.

--- client/src/test/kotlin/com/configset/client/ClientTestUtil.kt
+++ client/src/test/kotlin/com/configset/client/ClientTestUtil.kt
@@ -4,79 +4,30 @@ import com.configset.client.proto.ConfigurationServiceGrpc
 import com.configset.client.proto.PropertiesChangesResponse
 import com.configset.client.proto.PropertyItem
 import com.configset.client.proto.WatchRequest
+import com.configset.common.client.extension.createLoggerStatic
 import com.configset.common.client.grpc.ConfigSetClient
 import com.configset.common.client.grpc.DeadlineInterceptor
+import com.google.common.util.concurrent.ThreadFactoryBuilder
 import io.grpc.ManagedChannel
 import io.grpc.Server
 import io.grpc.inprocess.InProcessChannelBuilder
 import io.grpc.inprocess.InProcessServerBuilder
 import io.grpc.stub.StreamObserver
-import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicLong
-import kotlin.concurrent.thread
+import java.util.function.Consumer
+
+private val log = createLoggerStatic<ClientTestUtil>()
 
 class ClientTestUtil {
 
     lateinit var asyncClient: ConfigurationServiceGrpc.ConfigurationServiceStub
-    private var updateVersion = AtomicLong()
-    private val cmdQueue = LinkedBlockingQueue<Payload>()
+    private var updateVersion = AtomicLong(2)
     private lateinit var channel: ManagedChannel
     private lateinit var server: Server
 
-    private val service = object : ConfigurationServiceGrpc.ConfigurationServiceImplBase() {
-
-        @Volatile
-        var isShuttedDown = false
-
-        override fun watchChanges(responseObserver: StreamObserver<PropertiesChangesResponse>): StreamObserver<WatchRequest> {
-            return object : StreamObserver<WatchRequest> {
-                override fun onNext(value: WatchRequest) {
-                    when (value.type) {
-                        WatchRequest.Type.SUBSCRIBE_APPLICATION -> startSubscription(responseObserver)
-                        WatchRequest.Type.UPDATE_RECEIVED -> return
-                        else -> responseObserver.onError(RuntimeException("Unrecognized option received"))
-                    }
-                    if (value.type != WatchRequest.Type.SUBSCRIBE_APPLICATION) {
-                        return
-                    }
-                }
-
-                override fun onError(t: Throwable) {
-                    isShuttedDown = true
-                }
-
-                override fun onCompleted() {
-                    isShuttedDown = true
-                }
-
-                private fun startSubscription(responseObserver: StreamObserver<PropertiesChangesResponse>) {
-                    thread {
-                        val lastVersion = if (!cmdQueue.isEmpty() && cmdQueue.element() is Payload.Msg) {
-                            (cmdQueue.element() as Payload.Msg).data.lastVersion - 1
-                        } else {
-                            updateVersion.get()
-                        }
-                        responseObserver.onNext(
-                            PropertiesChangesResponse.newBuilder()
-                                .setApplicationName(APP_NAME)
-                                .setLastVersion(lastVersion)
-                                .build()
-                        )
-                        while (!isShuttedDown) {
-                            when (val cmd = cmdQueue.take()) {
-                                Payload.DropConnection -> {
-                                    responseObserver.onCompleted()
-                                    return@thread
-                                }
-
-                                is Payload.Msg -> responseObserver.onNext(cmd.data)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
+    private val service = FakeConfigService()
 
     fun start() {
         server = InProcessServerBuilder.forName("mytest")
@@ -95,7 +46,7 @@ class ClientTestUtil {
         propertyValue: String,
     ) {
         val version = updateVersion.incrementAndGet()
-        cmdQueue.add(
+        service.pushMessageAsync(
             Payload.Msg(
                 PropertiesChangesResponse.newBuilder()
                     .setApplicationName(appName)
@@ -119,7 +70,7 @@ class ClientTestUtil {
         propertyName: String,
     ) {
         val version = updateVersion.incrementAndGet()
-        cmdQueue.add(
+        service.pushMessageAsync(
             Payload.Msg(
                 PropertiesChangesResponse.newBuilder()
                     .setApplicationName(appName)
@@ -138,12 +89,106 @@ class ClientTestUtil {
     }
 
     fun dropConnection() {
-        cmdQueue.add(Payload.DropConnection)
+        service.pushMessageAsync(Payload.DropConnection)
     }
 
     fun stop() {
         channel.shutdown()
         server.shutdown()
+    }
+}
+
+private class FakeConfigService : ConfigurationServiceGrpc.ConfigurationServiceImplBase() {
+
+    private var subscribers = mutableListOf<Consumer<Payload>>()
+    private var msgLog = mutableListOf<Payload.Msg>()
+    private var executor = Executors.newCachedThreadPool(ThreadFactoryBuilder().setDaemon(true).build())
+
+    @Synchronized
+    fun pushMessageAsync(msg: Payload) {
+        if (msg is Payload.Msg) {
+            msgLog.add(msg)
+        }
+        executor.execute {
+            subscribers.forEach { it.accept(msg) }
+        }
+    }
+
+    @Synchronized
+    fun addSubscriber(t: Consumer<Payload>) {
+        subscribers.add(t)
+    }
+
+    override fun watchChanges(responseObserver: StreamObserver<PropertiesChangesResponse>):
+            StreamObserver<WatchRequest> {
+
+        val r = CompletingObserver(responseObserver, msgLog)
+        addSubscriber(r)
+        return r
+    }
+}
+
+private class CompletingObserver(
+    private val responseObserver: StreamObserver<PropertiesChangesResponse>,
+    private val msgLog: List<Payload.Msg>,
+) : StreamObserver<WatchRequest>, Consumer<Payload> {
+
+    @Volatile
+    private var completed = false
+    private lateinit var subscriber: Consumer<Payload>
+    private val subscribed = CountDownLatch(1)
+
+    override fun accept(t: Payload) {
+        subscribed.await()
+        subscriber.accept(t)
+    }
+
+    override fun onNext(value: WatchRequest) {
+        when (value.type) {
+            WatchRequest.Type.SUBSCRIBE_APPLICATION -> {
+                subscriber = Consumer<Payload> {
+                    if (completed) {
+                        return@Consumer
+                    }
+                    when (it) {
+                        is Payload.DropConnection -> {
+                            completed = true
+                            responseObserver.onCompleted()
+                        }
+
+                        is Payload.Msg -> responseObserver.onNext(it.data)
+                    }
+                }
+                startSubscription(subscriber)
+                subscribed.countDown()
+                for (payload in msgLog) {
+                    subscriber.accept(payload)
+                }
+            }
+
+            WatchRequest.Type.UPDATE_RECEIVED -> return
+            else -> responseObserver.onError(RuntimeException("Unrecognized option received ${value.type}"))
+        }
+    }
+
+    override fun onError(t: Throwable) {
+        log.warn("Exception in stream", t)
+        completed = true
+    }
+
+    override fun onCompleted() {
+        completed = true
+    }
+
+    private fun startSubscription(subscriber: Consumer<Payload>) {
+        subscriber.accept(
+            Payload.Msg(
+                PropertiesChangesResponse.newBuilder()
+                    .setApplicationName(APP_NAME)
+                    .setLastVersion(1)
+                    .build()
+            )
+        )
     }
 }
 
